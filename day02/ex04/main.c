2e37f6412b5efef8c9cf13362216aa27f3536aa6
Clara COTTIN <ccottin@f1br8s6.clusters.42paris.fr>
2025-10-30 18:50:16 +0100

    leds are epics (:::::

--- day02/ex04/main.c
+++ day02/ex04/main.c
@@ -4,8 +4,8 @@
 
 #define BAUD_PRESCALLER ((F_CPU / 16) + UART_BAUDRATE / 2) / UART_BAUDRATE - 1
 
-#define USERNAME "spectre"
-#define PASSWORD "coucou"
+#define USERNAME "spectre\0"
+#define PASSWORD "coucou\0"
 
 void    uart_init(void)
 {
@@ -27,7 +27,7 @@ void    uart_tx(char c)
  
 }
 
-void uart_printstr(const char* str)
+void    uart_printstr(const char* str)
 {
     int i;
 
@@ -40,36 +40,126 @@ void uart_printstr(const char* str)
     }
 }
 
-// Interrupt definition to capitalize, then send back chars when data is detected. 
-ISR(USART_RX_vect) 
+char    uart_rx(void)
 {
+    // Wait for RXC0 flag, meaning there is data received 
+     while(!(UCSR0A & (1 << RXC0))) { }
+     return (UDR0);
+}
+
+char    ft_strcmp(char *s1, char *s2)
+{
+    int i;
+
+    i = 0;
+    while (s1[i] && s2[i])
+    {
+        if (s1[i] == s2[i])
+            ++i;
+        else
+            return (0);
+    }
+    if (s1[i] == 0 && s2[i] == 0)
+        return (1);
+    return (0);
+}
+
+void    config(void) {
+    // Leds as output
+    DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB4);
+    DDRD |= (1 << DDD5) | (1 << DDD6) | (1 << DDD3);
+}
+
+void    enter_string(char* str, char masked)
+{
+    int     i;
     char    c;
 
-    c = UDR0;
-    if (c >= 'a' && c <= 'z') 
-    {
-        c -= 32;
+    i = 0;
+      while (i < 10) {
+        c = uart_rx();
+        if (c == '\r') // char send by screen when enter is used
+            break;
+        else if (c == 127)
+        {
+            if (i > 0) {
+                --i;
+                uart_tx(8);     // backspace
+                uart_tx(' ');   // print a space char
+                uart_tx(8);     // backspace to move cursor back
+            }
+        }
+        else {
+            str[i] = c;
+            if (masked) {
+                uart_tx('*');
+            } else {
+                uart_tx(c);
+            }
+            ++i;
+        }
     }
-    else if (c >= 'A' && c <= 'Z') 
-    {
-        c += 32;
-    }
-    uart_tx(c);
+    str[i] = 0;
 }
 
-void    config(void) 
+void    approuved(void)
 {
-    // Enable flag on rx data from uart
-    UCSR0B |= (1 << RXCIE0);
+    uart_printstr("\n\rHello there (:\n");
+    for (int i = 5 ; i > 0; i--) {
+        PORTB |= (1 << PORTB0);
+        PORTD |= (1 << PORTD3);
+        _delay_ms(100);
+        PORTB |= (1 << PORTB1);
+        PORTB &= ~(1 << PORTB0);
+        PORTD |= (1 << PORTD5);
+        _delay_ms(100);
+        PORTB |= (1 << PORTB2);
+        PORTB &= ~(1 << PORTB1);
+        PORTD &= ~(1 << PORTD3);
+        _delay_ms(100);
+        PORTB |= (1 << PORTB4);
+        PORTB &= ~((1 << PORTB2));
+        PORTD &= ~(1 << PORTD5);
+        PORTD |= (1 << PORTD6);
+        _delay_ms(100);
+        PORTD |= (1 << PORTD3);
+        PORTB &= ~((1 << PORTB4));
+        _delay_ms(100);
+    }
+    PORTB &= ~((1 << PORTD3) | (1 << PORTD5));
 }
 
-int main(void)
+void    reject(void) {
+    uart_printstr("\n\rBad combinaison username/password\n");
+    for (int i = 4 ; i > 0; i--) {
+        PORTB |= (1 << PORTB0) | (1 << PORTB1) | (1 << PORTB2) | (1 << PORTB4);
+        PORTD |= (1 << PORTD5);
+        _delay_ms(500);
+        PORTB &= ~((1 << PORTB0) | (1 << PORTB1) | (1 << PORTB2) | (1 << PORTB4));
+        PORTD &= ~((1 << PORTD5));
+        _delay_ms(500);
+    }
+}
+
+int     main(void)
 {
-    config();
+    char    username[11];
+    char    password[11];
+
     uart_init();
-    // Enable interrupts
-    sei();
+    config();
     while (42) {
-        uart_printstr("Enter your login:\nusername: ");
-     }
+        uart_printstr("\rEnter your login:\n\r\tusername: ");
+        enter_string(username, 0);
+        uart_printstr("\n\r\tpassword: ");
+
+        enter_string(password, 1);
+
+        if (ft_strcmp(USERNAME, username) && ft_strcmp(PASSWORD, password)) {
+            approuved();
+        }
+        else {
+            reject();
+        }
+    }
 }
