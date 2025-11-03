544e4d5b9a15c7fbdbe45741fece9d8f00a6a057
Clara COTTIN <ccottin@f1br5s8.clusters.42paris.fr>
2025-11-03 14:54:04 +0100

    lib cleans

---
+++ day04/ex00/lib_uart.h
@@ -0,0 +1,17 @@
+#ifndef LIB_UART_H
+# define LIB_UART_H
+
+# include <avr/io.h>
+# include <util/delay.h>
+# include <avr/interrupt.h>
+
+# define BAUD_PRESCALLER ((F_CPU / 16) + UART_BAUDRATE / 2) / UART_BAUDRATE - 1
+
+void    uart_init(void);
+void    uart_tx(char c);
+void    uart_printstr(const char* str);
+char    uart_rx(void);
+char    ft_strcmp(char *s1, char *s2);
+void    uart_getstr(char* str, char size);
+
+#endif
