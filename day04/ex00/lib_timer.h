6ba166e48db29c63f2566f0d1428976088dd89f2
Clara COTTIN <ccottin@f1br5s8.clusters.42paris.fr>
2025-11-03 14:51:38 +0100

    lib cleans

---
+++ day04/ex00/lib_timer.h
@@ -0,0 +1,8 @@
+#ifndef LIB_TIMER_H
+# define LIB_TIMER_H
+
+# include <avr/io.h>
+# include <util/delay.h>
+# include <avr/interrupt.h>
+
+#endif
--- day04/ex00/main.c
+++ day04/ex00/main.c
@@ -2,21 +2,34 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+volatile uint8_t is_pressed = 0;
+
 void    setup(void) {
     // Led as output
     DDRB |= (1 << DDB0);
-    // Setting flag for interrupt on pin change, on port D, where sw1 is solded
-    PCICR |= (1 << PCIE2);
-    PCMSK2 |= (1 << PCINT18);
+    // Setting flag for interrupt on int0, on port D, where sw1 is solded
+    EIMSK |= (1 << INT0);
+    // Interrupt will be triggered on falling edge of the button, meaning when button is pressed
+    EICRA |= (1 << ISC01);
 }
 
-ISR(PCINT0_vect) {
-    PORTB |= (1 << PORTB0);
+ISR(INT0_vect) {
+    // blink
+    is_pressed = 1;
+    // delay for debounce, it is not safe to put delay in interrupts when there is 
+    //more code, but since our only goal is to trigger the led this should be fine
 }
 
 int main(void) {
     setup();
     // enable interrupt
+
     sei();
-    while (42) { }
+    while (42) {
+        if (is_pressed == 1) {
+            _delay_ms(200);
+            PORTB ^= (1 << PORTB0);
+            is_pressed = 0;
+        }
+     }
 }
